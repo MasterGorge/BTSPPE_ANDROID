fae970147e6838c47da3746cefcc6cfe31c7ee0c
Rayan69100 <yayan1@live.fr>
2023-04-28 18:40:53 +0200

    ok

--- app/src/main/java/com/example/btsppe_android/Activities/FichesFraisActivity.java
+++ app/src/main/java/com/example/btsppe_android/Activities/FichesFraisActivity.java
@@ -1,4 +1,17 @@
 package com.example.btsppe_android.Activities;
 
-public class FichesFraisActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.btsppe_android.R;
+
+public class FichesFraisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiches_frais);
+    }
+
 }
