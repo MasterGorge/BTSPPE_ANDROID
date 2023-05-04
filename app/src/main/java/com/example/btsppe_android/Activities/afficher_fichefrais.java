fe326aad8a99890b9bb2a48eea579af426974047
MasterGorge <103949325+MasterGorge@users.noreply.github.com>
2023-05-04 18:19:49 +0200

    Martin

--- app/src/main/java/com/example/btsppe_android/Activities/afficher_fichefrais.java
+++ app/src/main/java/com/example/btsppe_android/Activities/afficher_fichefrais.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.btsppe_android.R;
+
 public class afficher_fichefrais extends AppCompatActivity {
 
     @Override
