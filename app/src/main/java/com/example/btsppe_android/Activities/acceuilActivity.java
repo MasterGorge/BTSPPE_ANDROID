795f8a561326b648e4a3099afff55b36f065d7b4
Rayan69100 <yayan1@live.fr>
2023-05-13 21:12:02 +0200

    ok

---
+++ app/src/main/java/com/example/btsppe_android/Activities/acceuilActivity.java
@@ -0,0 +1,16 @@
+package com.example.btsppe_android.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.btsppe_android.R;
+
+public class acceuilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
